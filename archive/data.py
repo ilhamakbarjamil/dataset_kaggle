bde82cf5c3a35fe19fcc6d86f0db2aadc18e8178
ilhamakbarjamil <ilhamakbarjamil8@gmail.com>
2025-01-02 17:56:30 +0700

    anjay

--- archive/data.py
+++ archive/data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
-iris_df = pd.read_csv('D:\\KULIAH\\bagas_stastika\\archive\\data_bersih.csv')
+iris_df = pd.read_csv(r'D:\KULIAH\bagas_stastika\archive\data_bersih.csv')
 iris_df.head()
-print(iris_df.head())
+
+
+print(iris_df.columns)
